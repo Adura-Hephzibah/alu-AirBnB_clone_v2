3799939788c6ee593ed74277578a15333004f553
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-03-07 12:21:33 +0200

    Task 0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-# LOCATION_VAR="location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n"
-# TARGET="location\s.~\s.\/.php"
-
 # Install Nginx if it not already installed
 sudo apt-get -y update
 sudo apt-get -y install nginx
@@ -23,7 +20,6 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown  -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-# sed -ri "s/$TARGET/$LOCATION_VAR/" /etc/nginx/sites-available/default
 sed -ri "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-available/default
 
 # restart Nginx after updating the configuration
